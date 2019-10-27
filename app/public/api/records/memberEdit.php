12f8eae288e095424dba88904f93e83ffe64d12d
raygairik <54493787+raygairik@users.noreply.github.com>
2019-10-27 12:59:32 -0400

    pushed memebr codes

--- app/public/api/records/memberEdit.php
+++ app/public/api/records/memberEdit.php
@@ -6,11 +6,28 @@ $db = DbConnection::getConnection();
 // Step 2: Create & run the query
 $stmt = $db->prepare(
   'UPDATE Member
-    set firstName = ?, lastName = ?
-    where memberID = ?');
+    set firstName = ?, lastName = ?, dob = ?,
+    Gender = ?, Email = ?, address = ?, City = ?,
+     State = ?, ZIPCode = ?, workPhoneNumber = ?,
+     mobilePhoneNumber = ? , departmentPosition = ?,
+     Radio = ?, Station = ?, isActive = ?
+     where memberID = ?');
 $stmt->execute([
   $_POST['firstName'],
   $_POST['lastName'],
+  $_POST['dob'],
+  $_POST['Gender'],
+  $_POST['Email'],
+  $_POST['address'],
+  $_POST['City'],
+  $_POST['State'],
+  $_POST['ZIPCode'],
+  $_POST['workPhoneNumber'],
+  $_POST['mobilePhoneNumber'],
+  $_POST['departmentPosition'],
+  $_POST['Radio'],
+  $_POST['Station'],
+  $_POST['isActive'],
   $_POST['memberID']
 ]);
 
